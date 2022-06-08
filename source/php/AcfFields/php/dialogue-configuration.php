759dbf4efb4ab310a2d551c7b4544846e9355f0e
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2022-06-08 14:13:34 +0200

    remove required

--- source/php/AcfFields/php/dialogue-configuration.php
+++ source/php/AcfFields/php/dialogue-configuration.php
@@ -11,7 +11,7 @@
             'name' => 'plane_dimensions_width',
             'type' => 'number',
             'instructions' => '',
-            'required' => 1,
+            'required' => 0,
             'conditional_logic' => 0,
             'wrapper' => array(
                 'width' => '50',
@@ -32,7 +32,7 @@
             'name' => 'plane_dimensions_height',
             'type' => 'number',
             'instructions' => '',
-            'required' => 1,
+            'required' => 0,
             'conditional_logic' => 0,
             'wrapper' => array(
                 'width' => '50',
